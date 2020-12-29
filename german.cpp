53086833037586bf92e33988439448c2c78d44e0
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2020-12-29 17:38:48 +0100

    Initial German Commit

---
+++ german.cpp
@@ -0,0 +1,6 @@
+#include "german.hpp"
+
+ganzZahl haupt()
+{
+    gebe_zurueck 0;
+}
---
+++ german.hpp
@@ -0,0 +1,43 @@
+#ifndef GERMAN_HPP
+#define GERMAN_HPP
+
+#define leer void
+#define ganzZahl int
+#define kleineGanzZahl short
+#define großeGanzZahl long
+#define buchstabe char
+#define wahrheitswert bool
+#define einfachPraezisionGleitkommaZahl float
+#define zweifachPraezisionGleitkommaZahl double
+#define verbund union
+#define struktur struct
+
+#define wenn if
+#define solange while
+#define fuer for
+#define tue do
+#define unterscheide switch
+#define fall case
+
+#define haupt main
+#define sei =
+
+#define gebe_zurueck return
+
+#define null 0
+#define eins 1
+#define zwei 2
+#define drei 3
+#define vier 4
+#define fuenf 5
+#define sechs 6
+#define sieben 7
+#define acht 8
+#define neun 9
+
+#define klasse class
+#define oeffentlich public
+#define privat private
+#define geschuetzt protected
+
+#endif
