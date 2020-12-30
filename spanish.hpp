e1f3c7526870451060043af66f7f4578e5058343
Hendrik Sauer <hendrik-sauer@gmx.de>
2020-12-30 01:15:35 +0100

    Add spanish

---
+++ spanish.hpp
@@ -0,0 +1,57 @@
+#ifndef SPANISH_HPP
+#define SPANISH_HPP
+
+#define vacío void
+#define entero int
+#define entero_pequeño short
+#define entero_grande long
+#define letra char
+#define valor_verdad bool
+#define simple_precisión_número_decimal float
+#define doble_precisión_número_decimal double
+#define unión union
+#define estructura struct
+
+#define si if
+#define otra else
+#define mientras while
+#define por for
+#define hace do
+#define diferenciar switch
+#define caso case
+#define termina break
+#define seguir continue
+#define va_a goto
+
+#define principal main
+#define ser =
+
+#define retornar return
+
+#define cero 0
+#define uno 1
+#define dos 2
+#define tres 3
+#define cuatro 4
+#define cinco 5
+#define seis 6
+#define siete 7
+#define ocho 8
+#define nueve 9
+#define diez 10
+#define once 11
+#define doce 12
+
+#define clase class
+#define pùblico public
+#define privado private
+#define protegido protected
+
+#define usar using
+#define espacio_de_nombre namespace
+
+#define dice cout
+#define escucha cin
+#define fin_de_la_linea endl;
+
+#endif
